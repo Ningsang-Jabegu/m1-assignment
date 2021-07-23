fd30eb4caf1e2af4ff26c3dc89280bdceafd83aa
Ningsang-Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2021-07-23 19:45:18 +0545

    Done: I am happy.

--- m1-assignment/mini.js
+++ m1-assignment/mini.js
@@ -1,27 +1,24 @@
-//Variable decleration
-var openList;
-var closeList;
-var fileNames = [];
 var photos = [];
+var fileNames = [];
 var imageList = [];
-var openCaptionTag ;
-var closeCaptionTag ;
-var openDescTag;
-var closeDescTag;
-//<li id="photo"><img src="images/photo2.jfif" class="image" alt="image2">
-//<figcaption>Kathmandu City</figcaption><p class="discription">This is Kathmandu City: an arial view.</p></li>
-openList= "<li id='photo'>";
-closeList= "</li>";
-openCaptionTag= "<figcaption>";
-closeCaptionTag= "</figcaption";
-openDescTag= "<p class='discription'>";
-closeDescTag= "</p>";
-for (let i=0; i<11; i++) {
-        fileNames += "photo"+i;
-        photos.push("<img src='images/"+fileNames+".jfif' class='image' >")
-
-        image = openList + photos + openCaptionTag + closeCaptionTag+ openDescTag + closeDescTag+ closeList;
+var image;
+var openList = "<li id='photo'>";
+var closeList = "</li>";
+var openCaptionTag ="<figcaption>";
+var closeCaptionTag ="</figcaption>";
+var openDescTag ="<p class='discription'>";
+var closeDescTag ="</p>";
+var captionTexts =[];
+var descTexts =[];
+for (var i=0; i<10; i++) {
+        fileNames.push("photo"+(i+1));
+        photos.push("<img src ='images/"+fileNames[i]+".jfif' class='image'alt='"+fileNames[i]+"'>");
+        captionTexts.push("Boudhanath Stupa","Kathmandu City","Thamel","Pashupatinath Temple","Local Store","White Gumba","Durbar Square","Dal Bhat","Lord Buddha Statue","Baba Ji");
+        descTexts.push("This is Boudhanath Stupa: an arial view.","This is Kathmandu City: an arial view.","This is Thamel: a street view.","Pashupatinath temple: World heritage site","This is local store: an local view.","This is White Gumba: a corner view."
+        ,"This is Durbar Square: a front view.","This is Dal Bhat: a local food.","This is statue of lord Buddha: a side view.","They are baba ji: a view.");
+        image = openList + photos[i]+ openCaptionTag +captionTexts[i]+ closeCaptionTag+openDescTag+descTexts[i]+closeDescTag+ closeList;
         imageList.push(image);
 }
-//Display all ten image codes stored in the array
+//<li id="photo" ><img src="images/photo1.jfif" class="image" alt="image1">
+//<figcaption>Boudhanath Stupa</figcaption><p class="discription">This is Boudhanath Stupa: an arial view.</p></li>
 document.getElementById("album").innerHTML = imageList;
