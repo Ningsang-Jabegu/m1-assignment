4eac933dc9c0110fe647d1ff5cda450e40e96303
Ningsang-Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2021-07-31 09:23:32 +0545

    updated

--- m1-assignment/mini.js
+++ m1-assignment/mini.js
@@ -1,66 +1,79 @@
+
+
+
 var photos = [];
 var fileNames = [];
 var imageList = [];
 var image;
-var openList = "<li id='photo' onclick='myFunction()'>";
+var openList = "<li id='photo' class='lightbox-toggle'>";
 var closeList = "</li>";
 var openCaptionTag ="<figcaption>";
 var closeCaptionTag ="</figcaption>";
-var openDescTag ="<p class='discription'>";
+var openDescTag ="<p class='discription'onclick='myFunction()'>";
 var closeDescTag ="</p>";
 var captionTexts =[];
 var descTexts =[];
 for (var i=0; i<10; i++) {
         fileNames.push("photo"+(i+1));
-        photos.push("<img src ='images/"+fileNames[i]+".jfif' class='image'alt='"+fileNames[i]+"' >");
+        photos.push("<img src ='images/"+fileNames[i]+".jfif' class='image lightbox-toggle'alt='"+fileNames[i]+"' >");
         captionTexts.push("Boudhanath Stupa","Kathmandu City","Thamel","Pashupatinath Temple","Local Store","White Gumba","Durbar Square","Dal Bhat","Lord Buddha Statue","Baba Ji");
         descTexts.push("This is Boudhanath Stupa: an arial view.","This is Kathmandu City: an arial view.","This is Thamel: a street view.","Pashupatinath temple: World heritage site","This is local store: an local view.","This is White Gumba: a corner view."
         ,"This is Durbar Square: a front view.","This is Dal Bhat: a local food.","This is statue of lord Buddha: a side view.","They are baba ji: a view.");
         image = openList + photos[i]+ openCaptionTag +captionTexts[i]+ closeCaptionTag+openDescTag+descTexts[i]+closeDescTag+ closeList;
         imageList.push(image);
 }
-//<li id="photo" ><img src="images/photo1.jfif" class="image" alt="image1">
-//<figcaption>Boudhanath Stupa</figcaption><p class="discription">This is Boudhanath Stupa: an arial view.</p></li>
+
 document.getElementById("album").innerHTML = imageList;
 
-// Info Box
-
-       
+// Info Box   
 function myFunction() {
         var OpenHeadingTag ="<h3 id='heading'>";
         var closeHeadingTag ="</h3>";
-        var heading = [];
+        var heading = [
+                "Boudhanath Stupa",
+                "Kathmandu City",
+                "Thamel",
+                "Pashupatinath Temple",
+                "Local Store",
+                "White Gumba",
+                "Durbar Square",
+                "Dal Bhat",
+                "Lord Buddha Statue",
+                "Baba Ji"
+        ];
         var openTextInfoTag ="<p id='textInfo'>";
-        var textInfo = [];
+       var textInfo = ["This is Boudhanath Stupa: an arial view.",
+        "This is Kathmandu City: an arial view.",
+        "This is Thamel: a street view.",
+        "Pashupatinath temple: World heritage site",
+        "This is local store: an local view.",
+        "This is White Gumba: a corner view.",
+        "This is Durbar Square: a front view.",
+        "This is Dal Bhat: a local food.",
+        "This is statue of lord Buddha: a side view.",
+        "They are baba ji: a view."];
         var closeTextInfoTag ="</p>";
         var opentextLinkTag ="<p id='textLink' onclick='myFunction1()'>";
         var textLink = [];
         var closeTextLinkTag ="</p>";
         var inforBox;
+        var head;var text;
         textLink.push("Click this to close");
-        for (var i=0; i<=10; i++){
-
-        heading.push("Boudhanath Stupa","Kathmandu City","Thamel","Pashupatinath Temple","Local Store","White Gumba","Durbar Square","Dal Bhat","Lord Buddha Statue","Baba Ji");
-        textInfo.push("This is Boudhanath Stupa: an arial view.","This is Kathmandu City: an arial view.","This is Thamel: a street view.","Pashupatinath temple: World heritage site","This is local store: an local view.","This is White Gumba: a corner view."
-                ,"This is Durbar Square: a front view.","This is Dal Bhat: a local food.","This is statue of lord Buddha: a side view.","They are baba ji: a view.");
-        
-        var head = heading[i+1];
-        var body = textInfo[i+1];
-        /*
-        if photo1=
-        */    
-                inforBox = OpenHeadingTag + head +closeHeadingTag +openTextInfoTag +body +
+        for (var i=0; i<10; i++) { 
+                heading[i] = head;
+                textInfo[i] = text;
+                if(i>=0) {
+                inforBox = OpenHeadingTag + head +closeHeadingTag +openTextInfoTag +text +
                 closeTextInfoTag + opentextLinkTag + textLink + closeTextLinkTag; 
-
+                document.getElementById("infoBox").style.visibility ="visible";
+                document.getElementById("infoBox").innerHTML = inforBox;
+                }
         } 
-        document.getElementById("infoBox").style.visibility ="visible";
-        document.getElementById("infoBox").innerHTML = inforBox;
-        
-
       }
 function myFunction1() {
         document.getElementById("infoBox").style.visibility ="hidden";
 }
+
 // dropdown menu
 function contact() {
      var select_content = document.getElementById ("select_contact");
@@ -81,3 +94,6 @@ else {
         document.getElementById("hide-unhide") = getFaxLabel + getFaxBox;
 }
 }
+
+
+        
